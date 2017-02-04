d44c7d06cad49fa6e548b05ecac6cacb6b4eff08
alexts <724577966@qq.com>
2017-02-04 17:59:03 +0800

    added js, php file and modified html file

---
+++ left-nav.php
@@ -0,0 +1,32 @@
+<!--left-nav-->
+<ul>
+    <li>
+		<a href="index.html">
+		<i class="icon icon-home"></i> 
+		<span>Dashboard</span></a> 
+	</li>
+    <li>
+		<a href="charts.html">
+		<i class="icon icon-signal"></i> 
+		<span>Charts &amp; graphs</span></a> 
+	</li>
+    <li>
+		<a href="widgets.html">
+		<i class="icon icon-inbox"></i>
+		<span>Widgets</span></a>
+	</li>
+	<li class="active">
+		<a href="grid.html">
+		<i class="icon icon-fullscreen"></i> 
+		<span>Full width</span></a>
+	</li>
+    <li><a href="tables.html"><i class="icon icon-th"></i> <span>Tables</span></a></li>    
+    <li class="content"> <span>Disk Space Usage</span>
+      <div class="progress progress-mini active progress-striped">
+        <div style="width: 87%;" class="bar"></div>
+      </div>
+      <span class="percent">87%</span>
+      <div class="stat">604.44 / 4000 MB</div>
+    </li>
+  </ul>
+  <!--left-nav-->
